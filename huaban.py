0367687d2effc6e79ed39d094626b124fcf0f717
donghao <Conquer_you@163.com>
2025-12-03 10:44:49 +0800

    适配当前花瓣网版本

--- huaban.py
+++ huaban.py
@@ -1,77 +1,79 @@
 import time
+import urllib3
 import requests
 import json
 import re
 import os
-
+# 禁用 InsecureRequestWarning 警告
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class HuaBan(object):
 
     def __init__(self):
-        self.url = "https://huaban.com/search/?"
+        self.url = "https://huaban.com/v3/search/file?"
         self.headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
-        'Referer': 'https://huaban.com/search/?q=%E5%8F%A4%E8%A3%85',
-        'Cookie': 'flowerCount=1; referer=https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3D0770Pt_gXKcIqPK3zApC5XVkzU41scgLRkc7QWGhINC%26wd%3D%26eqid%3D8805a1c1000011e00000000360a1de37; sid=s%3Aan7d3r566BUGHCOfNX-eh3-jyZNDt1hK.X1ABiqbWnjrLud4QVRlk90YoXgStX25lJsuMyRmmW4I; _f=iVBORw0KGgoAAAANSUhEUgAAADIAAAAUCAYAAADPym6aAAACuUlEQVRYR%2B2WT0gUURzHP287FCpKhyQINZIOmUGQk0jBjBERGd3yICQFSXQITYKsQwcvegjapVvboQw8FBFEWUS1sxCIrh2CtCCMTAjJCoJYpNAXv9lZWNfdcdRZSWhOD9533vt%2Bfv9mFFprvB6llOf%2BKm2Gf4Q9fSoBKUomiba1OZbaIxEi7e3Oui0aJVlc7AmioQiIAjcUxAvF5QtELjfjKQ9x05y3xkdGNOwAhHjknwPZMDNDa18fLf39FjAK3AYuABNuBlqAfkkcUAU0AFeAKeAokMyhk%2F1S4Ehap%2BCbH%2FglZcS2xHPqsWybr%2BXljNXUGBJpDceBt0C57EsZ6ZShYWATcM4FNTKNeeia5DwF7wIHyS6tPCDTwCOgHhhyoy8gTmm5ZVYLxPLoahXcSwdmNUGa3YsvAw8AMSmZkRKTUrnqZiRt0HQjLJnz0jkZDhQkc2pd6unhbnMz49XVdPX28rmyUqIu0X8KvAGeALYLMgCUAd3AKeBiRt9IiWXrpH%2F2ASeBW7KvoCvQ0sp7mI%2Bp5cfISjW%2Bm%2F0%2FyEpD7fP9gmREx4cOoFWf40HpVvS66zBXJmtl1r%2FM9qbtkVHQNaDGlFW3M593L102iPllGx9LvzNZ8jNlY7n%2FWtoe7ga1W1nGMW0nzjuHWca1hRCOrklZxh5tJ16DfqysvTLt5j3ueXl1aZCKX2UcmtzuvPus4kMQIIkTKM4o09ivYyNhQrMPc2YjnnjOHJ9Uo3FaxxI3CbFVmcbBBSCL6DIzIjANU1UMbp5YOYgY0fHEKzT30apKNdZ15CoZvSZAJMLK%2Bc8ayFVWLuwayEiqT84Cncoy7uTMSEoTWI%2FIHQUoLZleoQ5peK8pmj2NnF5R%2BnD2lPPSRXYNvkhDSLOX%2FFnP79AssS3jvN84vfyp5XP8ByYryHckMHdLOGgxkL8g0%2BMoiUoFIQAAAABJRU5ErkJggg%3D%3D%2CWin32.1920.1080.24; _uab_collina=162122092106443335994661; UM_distinctid=179784c0f3246b-0c8a5ebb168302-2363163-1fa400-179784c0f33916; CNZZDATA1256903590=1392307910-1621216776-https%253A%252F%252Fwww.baidu.com%252F%7C1621216776; __asc=d8fba831179784c12b51c588f6d; __auc=d8fba831179784c12b51c588f6d; __gads=ID=309f5f92dbe484cb:T=1621220926:S=ALNI_MaqK6qgKTDxsF9_Wwtyi1cM7sZGdg; CNZZDATA1279796125=1037052495-1621219298-https%253A%252F%252Fhuaban.com%252F%7C1621219298; _ga=GA1.2.870236201.1621220934; _gid=GA1.2.1392806091.1621220934; BAIDU_SSP_lcr=https://www.baidu.com/link?url=YcrkUOs6trKqtX6ITCck5NOr8kwDSKIljxjF5-hYI-K&wd=&eqid=91f79901000018a60000000360a1dee6; Hm_lvt_d4a0e7c3cd16eb58a65472f40e7ee543=1621220921,1621220974,1621221095; uid=30893018; _cnzz_CV1256903590=is-logon%7Clogged-in%7C1621221159253%26urlname%7Cy3qkysnq5z%7C1621221159253; Hm_lpvt_d4a0e7c3cd16eb58a65472f40e7ee543=1621221158'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
+            'Referer': 'https://huaban.com/',
+            'Cookie': 'user_device_id=c55d5c38b0c44e96baa059ad25d56d24; fd_id=e18f34fa4f53e18c9e32e13fe432adbc; fd_id_timestamp=1752054586031; canary_uuid=bc9c3e4570125bd78501dd4eb0cc4a90; canary=.?base; huaban-page-setting={%22cols%22:9%2C%22columnSize%22:240}; Hm_lvt_d4a0e7c3cd16eb58a65472f40e7ee543=1763516607,1764668270; HMACCOUNT=EE50C3B70B3CC054; _clck=1eivfn6%5E2%5Eg1i%5E0%5E2128; locale=zh-cn; token.prod=eyJ1c2VyX2lkIjozMDE1MTkwNTUsImh1YWJhbl9pZCI6MzgzMzIyNTcsInVzZXIiOjMwMTUxOTA1NSwiYWNjZXNzX3Rva2VuIjoiZXlKMGVYQWlPaUpxZDNRaUxDSmhiR2NpT2lKSVV6STFOaUo5LmV5SnBjM01pT2lKMWJYTWlMQ0p6ZFdJaU9qTXdNVFV4T1RBMU5Td2lZWFZrSWpvaWFIVmhZbUZ1SWl3aVpYaHdJam94TnpZME9USTNOalF6TENKMVkybGtJam9pT1RBek1UVXhOVEF6TURVME1ETTJPVGt6T1NJc0ltcDBhU0k2SWpGeGFuWnhlWGRwWlc4aUxDSm9ZbDlwWkNJNklqTTRNek15TWpVM0luMC5JT1Z5LXk4V0lvOWdGN00tYS1xU2tHdkh4eW05WU5wQjJYb0RMM3JsazRBIiwicmVmcmVzaF90b2tlbiI6Ik56d1U1aXI5Uk1Lb1R2UUEwd0ZnREEiLCJhY2Nlc3NfdG9rZW5fZXhwaXJlc19hdCI6IjIwMjUtMTItMDVUMDk6NDA6NDMuMDAwWiIsInJlZnJlc2hfdG9rZW5fZXhwaXJlc19hdCI6IjIwMjYtMDEtMDFUMDk6NDA6NDMuNzY4WiIsImFjY2Vzc190b2tlbl9saWZlX3RpbWUiOjI1OTIwMCwicmVmcmVzaF90b2tlbl9saWZlX3RpbWUiOjI1OTIwMDAsInRpbWVzdGFtcCI6MTc2NDY2ODQ0Mzg1MywibWVyZ2VkIjp0cnVlfQ==; token.prod.sig=KbR-4FaJBFbrxkRpZpows4WXSWJR_cVBA50rAdhQYkI; referer=https%3A%2F%2Fwww.gaoding.art%2F; sid=s%3Aikm4IH2k6uD83u2uwVMyc6FBrIgFwOHM.q10l6zu3VE%2BSIzCWXMwRMDAh4l8C%2FpX%2FCvUokUDE0Z4; uid=38332257; gd_id=2026463727255459901; token.org_id.prod=8031514056138794062; ab_test_id=40e5e42da030d25e9ecc52cddccf1e5b; feature_page=%257B%2522all%2522%253A5%257D; Hm_lpvt_d4a0e7c3cd16eb58a65472f40e7ee543=1764668454; _clsk=10eoxhy%5E1764668594776%5E14%5E0%5En.clarity.ms%2Fcollect',
+            'guest-token': 'c55d5c38b0c44e96baa059ad25d56d24',
+            'Accept': 'application/json, text/plain, */*',  # 修正为大写
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'X-Csrf-Token': '0fab09599529e9c612ecb2a51efc',
+            'Origin': 'https://huaban.com'
         }
         self.session = requests.session()
 
 
-    def params_one(self, page):
+    def params_one(self, img_type,page_num,page):
         params = {
-            "q": self.c,
-            "ki4kgpwa": "",
-            "page": page,
-            "per_page": "20",
-            "wfl": "1",
+            "text": img_type, # 关键字
+            "sort": "all",
+            "limit": page_num,  # 每页显示图片个数
+            "page": page, # 爬取第几页
+            "position": "search_pin",
+            "fields": "pins:PIN|total,facets,split_words,relations,rec_topic_material,topics",
         }
         return params
 
     def index_spider(self, params_ret):
-        response = self.session.get(url=self.url,params=params_ret, headers=self.headers).text
-        demo = ''.join(re.findall('pins"] = (.*);', response))
-        haha = json.loads(demo)
-        for demo in haha:
-            item = {}
-            item['demo'] = demo
-            yield item
+        response = self.session.get(url=self.url,params=params_ret, headers=self.headers,timeout=15,verify=False).text
+        data = json.loads(response)
+        data_list = data["pins"]
+        img_list= []
+        for i in data_list:
+            if 'file' in i:
+                url = "https://gd-hbimg.huaban.com/" + i['file']['key'] + "_fw480webp"
+            img_list.append(url)
+        return img_list
 
 
 
-    def save_data(self, index_item):
-        aaa = 0
-        for item in index_item:
-            tupian = item['demo']["file"]["key"]
-            tupian_url = "https://hbimg.huabanimg.com/" + tupian + "_fw658/format/webp"
-            xiazai = requests.get(tupian_url)
-            aaa += 1
-            try:
-                with open("图片爬取存储位置/" + tupian + ".jpg","wb") as a:
-                    print("正在抓取第%s张图片"%aaa)
-                    a.write(xiazai.content)
-            except:
-                print("抓取失败！")
+    def save_data(self, img_urls):
+        b = 0
+        for i in img_urls:
+            b += 1
+            xiazai = requests.get(url=i, headers=self.headers,timeout=15,verify=False)
+            img_name = str(img_urls[0]).split("/")[-1].replace("webp", ".webp", 1)
+            print(img_name)
+            with open("图片爬取存储位置/" + img_name ,"wb") as a:
+                print("正在下载第" + str(b) + "张图片：" + img_name)
+                a.write(xiazai.content)
 
     def main(self):
-        try:
-            self.c = input("请输入抓取图类型：")
-            a = int(input("请输入开始页："))
-            b = int(input("请输入结束页："))
-            if not os.path.exists("图片爬取存储位置"):
-                print("正在创建文件夹...")
-                os.makedirs("图片爬取存储位置")
-                time.sleep(2)
-            for page in range(a,b):
-                # page = 5
-                params_ret = self.params_one(page)
-                index_item = self.index_spider(params_ret)
-                self.save_data(index_item)
-                print("第%s页图片爬取完毕！" %page)
-            print("\n\n图片已爬取完毕！！！程序将自动关闭~")
+        img_type = input("请输入抓取图类型：")
+        page_num = input("请输入每页爬取的个数：")
+        page = input("请输入爬取第几页：")
+        if not os.path.exists("图片爬取存储位置"):
+            print("正在创建文件夹...")
+            os.makedirs("图片爬取存储位置")
             time.sleep(2)
-        except:
-            print("根据相关法律及政策，该搜索结果将不予显示，换个词试试吧！")
-            time.sleep(5)
+        params_ret = self.params_one(img_type,page_num,page)
+        img_urls = self.index_spider(params_ret)
+        self.save_data(img_urls)
+        time.sleep(2)
 
 
 if __name__ == '__main__':
